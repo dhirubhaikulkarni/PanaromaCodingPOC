f8dad1cda282ad626094ae21f87a82e6eb1fc824
dhirubhai.kulkarni@rhealtech.com <dhirubhai.kulkarni@rhealtech.com>
2024-05-26 20:44:49 +0530

    Added Authenticated File With JWT Verify method.

---
+++ API/Authentication/Authentication.js
@@ -0,0 +1,32 @@
+const jwt = require("jsonwebtoken");
+var jwkToPem = require('jwk-to-pem');
+var axios = require('axios');
+
+
+async function Authentication(req, res, next) {
+    console.log("Authentication")
+  try {
+    var token = getToken(req, res);
+    jwt.verify(token, process.env.JWT_PRIVATEKEY, async function (err, decoded) {
+      if (err) {
+        res.status(401).send('TokenExpire');
+      } else {
+        next();
+      }
+    });
+  } catch (ex) {
+    res.status(401).send('TokenExpire');
+  }
+}
+
+const getToken = (req, res) => {
+  if (req.headers.authorization && (req.headers.authorization).split(' ')[0] === 'Bearer') {
+    return (req.headers.authorization).split(' ')[1];
+  } else if (req.query && req.query.token) {
+    return req.query.token;
+  }
+  return res.status(401).send('TokenExpire');
+};
+
+module.exports.Authentication = Authentication;
+
